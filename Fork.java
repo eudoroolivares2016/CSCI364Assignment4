1ab2eade4ca8a7b8e5efe90c2e165a239fc3e4fd
Ed A Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2019-04-01 23:21:06 -0500

    fixed now should be working

---
+++ Fork.java
@@ -0,0 +1,17 @@
+//Eudoro Olivares 2/13/2018
+//package com.google.philosophers;
+public class Fork {
+	private boolean inUse = false;
+	public Fork() {
+	}
+	public boolean getUseStatus() {
+		return inUse;
+	}
+	public void grab() {
+		inUse = true;
+	}
+	
+	public void drop() {
+		inUse = false;
+	}
+}
